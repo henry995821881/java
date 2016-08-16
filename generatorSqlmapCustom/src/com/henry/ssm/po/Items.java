65cd17643e70f1e1fe7e3adb312aa5d2c326a5b1
henry <995821881@qq.com>
2016-08-16 23:36:19 +0800

    sqlmapcustom

---
+++ generatorSqlmapCustom/src/com/henry/ssm/po/Items.java
@@ -0,0 +1,55 @@
+package com.henry.ssm.po;
+
+import java.util.Date;
+
+public class Items {
+    private Integer id;
+
+    private Date createTime;
+
+    private String itemName;
+
+    private Float itemPrice;
+
+    private String itemDetail;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName == null ? null : itemName.trim();
+    }
+
+    public Float getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(Float itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public String getItemDetail() {
+        return itemDetail;
+    }
+
+    public void setItemDetail(String itemDetail) {
+        this.itemDetail = itemDetail == null ? null : itemDetail.trim();
+    }
+}
